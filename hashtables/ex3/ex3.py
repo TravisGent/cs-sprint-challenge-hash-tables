c55d07c90f0913d2ca06a8665e8807c89d9326a8
Eorium <travisgent312@gmail.com>
2020-10-02 14:09:19 -0600

    add ex3 answer

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,10 +2,28 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    result = []
+    newDictionaryArray = []
+    i = 0
+    while i < len(arrays):
+        newDict = {}
+        for item in arrays[i]:
+            newKey = str(item)
+            newDict[newKey] = item
+        newDictionaryArray.append(newDict)
+        i += 1
+
+    for item in arrays[0]:
+        y = 0
+        counter = 0
+        while y < len(newDictionaryArray):
+            if str(item) in newDictionaryArray[y]:
+                counter += 1
+            y +=1
+        if (counter == len(arrays)):
+            result.append(item)
 
     return result
-
 
 if __name__ == "__main__":
     arrays = []
--- pythontestingthings.py
+++ pythontestingthings.py
@@ -1,4 +1,9 @@
 x = 102
-y = 5
+y = "1"
 
-print(x % y)
+arrays = []
+
+arrays.append(list(range(10, 20)) + [1, 2, 3])
+arrays.append(list(range(20, 30)) + [1, 2, 3])
+arrays.append(list(range(30, 40)) + [1, 2, 3])
+print(arrays)
