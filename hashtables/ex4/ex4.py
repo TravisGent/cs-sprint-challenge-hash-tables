e11987c795a3fd85d35a839b742df4b764501952
Eorium <travisgent312@gmail.com>
2020-10-02 14:23:47 -0600

    add ex4 answer

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,15 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+
+    newDict = {}
+    for item in a:
+        newDict[str(item)] = item
+
+    for item in a:
+        if "-" + str(item) in newDict:
+            result.append(item)
 
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
